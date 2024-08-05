a71b4534461fc59a555c9df86850dcbe6d872a94
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-08-06 04:21:47 +0800

    update

--- devices/bcm53xx/diy.sh
+++ devices/bcm53xx/diy.sh
@@ -8,6 +8,5 @@ sed -i "s/^TARGET_DEVICES /# TARGET_DEVICES /" target/linux/bcm53xx/image/Makefi
 sed -i "s/# TARGET_DEVICES += phicomm_k3/TARGET_DEVICES += phicomm_k3/" target/linux/bcm53xx/image/Makefile
 sed -i "s/# TARGET_DEVICES += asus_rt-ac88u/TARGET_DEVICES += asus_rt-ac88u/" target/linux/bcm53xx/image/Makefile
 sed -i "s/# TARGET_DEVICES += dlink_dir-885l/TARGET_DEVICES += dlink_dir-885l/" target/linux/bcm53xx/image/Makefile
-sed -i "s/DEVICE_PACKAGES := \$(BRCMFMAC_4366C0) \$(USB3_PACKAGES)/DEVICE_PACKAGES := \$(BRCMFMAC_4366C0) \$(USB3_PACKAGES) -brcmfmac-firmware-4366c0-pcie brcmfmac-firmware-4366c0-pcie-k3 luci-app-k3screenctrl/" target/linux/bcm53xx/image/Makefile
+sed -i "s/DEVICE_PACKAGES := \$(BRCMFMAC_4366C0) \$(USB3_PACKAGES)/DEVICE_PACKAGES := \$(BRCMFMAC_4366C0) \$(USB3_PACKAGES) -brcmfmac-firmware-4366c0-pcie k3wifi luci-app-k3screenctrl/" target/linux/bcm53xx/image/Makefile
 
-git_clone_path master https://github.com/immortalwrt/immortalwrt package/firmware/brcmfmac4366c0-firmware-k3
